0f2bb11329d8ec2b42e9e8138ba4e4c8de8812e8
UmakanthD <ud@gmail.com>
2023-03-01 16:06:24 +0530

    adding Enums, generics files

---
+++ Enums/Enums/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public enum pool
+{
+   p, // as p is first letter , so it holds value =0;
+   q, // q value will be , q = p+1 ,Hence q = 1;
+   r, // similarly, r = p+2;
+
+   s=15, // as we are changing s value , so value(s) will be 15.
+   t,    // t holds 16;
+   v=20  // v holds 20;
+}
+namespace Enums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Names - Values");
+            string[] str = (string[])Enum.GetNames(typeof(pool));
+            
+            int[] values = (int[])Enum.GetValues(typeof(pool));
+
+            for(int i= 0;i < str.Length;i++)
+            {
+                Console.WriteLine("   "+str[i] + " - " + values[i]);
+            }
+
+            // accessing single value from enums.
+
+            Console.WriteLine("Using enum class name - accessing Names-values");
+            pool pp = new pool();
+
+            Console.WriteLine(pool.t + " " + (int)pool.t);
+            Console.WriteLine(pool.v + " " + (int)pool.v);
+
+        }
+    }
+}
---
+++ Generics in csharps/Generics in csharps/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public class calculate
+{
+    public bool areEqual<T>(T x,T y) // making function generic
+    {
+        if(x.Equals(y))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public  void swap<T>(ref T x,ref T y)
+    {
+        T temp = x;
+        x = y;
+        y = temp;
+    }
+
+}
+public class Maths<T> // by this way , we make class to Generic.
+{
+   
+
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("------------Generics---------------");
+        calculate c = new calculate();
+        int x = 30, y = 60;
+        Console.WriteLine("Before swapping x={0} y={1}", x, y);
+        c.swap(ref x,ref y);
+        Console.WriteLine("After swapping x={0} y={1}", x, y);
+        if(c.areEqual(45, 90))
+        {
+            Console.WriteLine("Equal");
+        }
+        else
+        {
+            Console.WriteLine("Not Equal");
+        }
+        if(c.areEqual<char>('M','M'))
+        {
+            Console.WriteLine("Equal");
+        }
+        else
+        {
+            Console.WriteLine("Not Equal");
+        }
+        char ch1 = 'P', ch2 = 'p';
+        
+        Console.WriteLine("Before swapping x={0} y={1}", ch1, ch2);
+        c.swap<char>(ref ch1, ref ch2);
+        Console.WriteLine("After swapping x={0} y={1}", ch1, ch2);
+    }
+}
+
